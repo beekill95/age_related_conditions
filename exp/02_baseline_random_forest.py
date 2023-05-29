faedb44a9c4baec0b6a4a8ec83c7b81addb27238
beekill <nguyenmbquan95@gmail.com>
2023-05-29 15:32:31 -0700

    Plot the results of cross validation

--- exp/02_baseline_random_forest.py
+++ exp/02_baseline_random_forest.py
@@ -14,10 +14,11 @@
 # ---
 
 # %%
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate
-from sklearn.preprocessing import OneHotEncoder
 
 # %% [markdown]
 # # Random Forest Baseline
@@ -43,4 +44,52 @@ X = X.fillna(medians)
 
 # %%
 rf = RandomForestClassifier(class_weight='balanced')
-cross_validate(rf, X=X, y=y, cv=6, scoring=['recall', 'precision', 'f1'], return_train_score=True)
+results = cross_validate(rf, X=X, y=y,
+                         cv=10,
+                         scoring=['recall', 'precision', 'f1'],
+                         return_train_score=True)
+results
+
+
+# %%
+# Plot cross validation results.
+def plot_cross_validation_results(results,
+                                  metrics: list[str], *,
+                                  train_prefix: str = 'train_',
+                                  test_prefix: str = 'test_'):
+    def transform_df_structure(df, type, type_prefix):
+        type_metrics = [f'{type_prefix}{m}' for m in metrics]
+
+        type_df = df[['Fold', *type_metrics]]
+        type_df = pd.melt(type_df,
+                       id_vars='Fold',
+                       value_vars=type_metrics,
+                       var_name='Metric',
+                       value_name='Score')
+        type_df['Metric'] = type_df['Metric'].replace({
+            k:v for k, v in zip(type_metrics, metrics)
+        })
+        type_df['Data'] = type
+
+        return type_df
+
+
+    # Convert `results` from dict to pandas' DataFrame.
+    df = pd.DataFrame(results)
+    df['Fold'] = df.index
+
+    train_df = transform_df_structure(df, 'Train', train_prefix)
+    test_df = transform_df_structure(df, 'Test', test_prefix)
+    df = pd.concat([train_df, test_df])
+
+    # Print the average for each metric.
+    for data_type in ['Train', 'Test']:
+        for m in metrics:
+            mdf = df[(df['Data'] == data_type) & (df['Metric'] == m)]
+            print(f'{data_type=}, metric={m}\n', mdf['Score'].describe())
+
+    # Plot the result.
+    sns.catplot(df, x='Fold', y='Score', hue='Metric', col='Data', kind='bar')
+
+
+plot_cross_validation_results(results, ['f1', 'precision', 'recall'])
